c6d74c31865d9a0df7d883ac276440766262c7fd
Victor Landim <victorlandim5@gmail.com>
2019-08-12 11:11:08 -0300

    Styled components in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-styled-components`
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        minify: false
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
