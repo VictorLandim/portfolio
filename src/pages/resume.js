51fd82820a0beef5ff45a4b070607bd628ae5250
Victor Landim <victorlandim5@gmail.com>
2019-10-18 10:20:38 -0300

    Linkedin link updated.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -22,7 +22,7 @@ const resume = {
   email: 'hi@victorlandim.com',
   website: 'victorlandim.com',
   github: 'https://github.com/VictorLandim',
-  linkedin: 'https://www.linkedin.com/in/victor-landim-95080b135/',
+  linkedin: 'https://www.linkedin.com/in/victor-landim/',
   skills: [
     {
       title: 'Front end',
