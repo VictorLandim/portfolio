617bfacaaf6b823560e95209a20a8c70b7a6dc56
Victor Landim <victorlandim5@gmail.com>
2020-01-22 00:34:59 -0300

    Project card updated.

--- src/components/sections/Projects/Project.js
+++ src/components/sections/Projects/Project.js
@@ -36,7 +36,7 @@ const Container = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.6);
+    background-color: rgba(0, 0, 0, 0.4);
     transition: 0.3s all;
   }
 
